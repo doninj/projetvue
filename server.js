d3b9237ce79605221a9bd7f0bf1ebb88a7218647
doninj <jean-baptiste.38@hotmail.com>
2021-06-16 16:29:59 +0200

    modifiy server

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const path = require('path');
 const app = express();
 const port = 3080;
 const history = require('connect-history-api-fallback');
+const cors = require('cors')
+const staticFileMiddleware = express.static('dist');
 
+app.use(cors())
+
+app.get('/api/lol', cors(), (req,res) => {
+  res.json({'test':'test'})
+})
 app.use(history());
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(staticFileMiddleware);
 app.listen(port, () => {
   console.log(`Server listening on the port::${port}`);
-});
-app.use(function (req, res) {
-  res.status(404).send('Error 404!!!');
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pre>{{ data }}</pre>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      data : {}
+    }
+  },
+  created () {
+    axios.get('/api/lol')
+    .then((response) => {
+      this.data = response.data
+    })
+    }
+}
+</script>
